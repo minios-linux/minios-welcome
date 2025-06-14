1ee11ffaca671c63fe503bf8b82593d5efa05ec0
crims0n <crims0n@yandex.ru>
2025-06-15 00:05:06 +0300

    Add Indonesian and Portuguese translations for MiniOS

---
+++ html/js/translations/id.js
@@ -0,0 +1,10 @@
+window.translations = {
+    "MiniOS - Fast. Simple. Reliable.": "MiniOS - Cepat. Sederhana. Andal.",
+    "Website": " Situs",
+    "News": " Berita",
+    "Community": " Komunitas",
+    "Documentation": " Dokumentasi",
+    "Source code": " Kode sumber",
+    "Thank you for choosing MiniOS.": "Terima kasih telah memilih MiniOS.",
+    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Kami berharap Anda menikmati sistem kami. Kami berusaha membuat MiniOS indah, sederhana, dan nyaman untuk Anda."
+};
---
+++ html/js/translations/pt_BR.js
@@ -0,0 +1,10 @@
+window.translations = {
+    "MiniOS - Fast. Simple. Reliable.": "MiniOS - Rápido. Simples. Confiável.",
+    "Website": " Site",
+    "News": " Notícias",
+    "Community": " Comunidade",
+    "Documentation": " Documentação",
+    "Source code": " Código fonte",
+    "Thank you for choosing MiniOS.": "Obrigado por escolher o MiniOS.",
+    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Esperamos que você goste do nosso sistema. Tentamos tornar o MiniOS bonito, simples e conveniente para você."
+};
