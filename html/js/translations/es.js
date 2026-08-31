64e8b671d44732359dda6f0f9a4c7a9616764f23
crim50n <crims0n@yandex.ru>
2026-09-01 01:31:52 +0300

    Release MiniOS Welcome 6.0.2

--- html/js/translations/es.js
+++ html/js/translations/es.js
@@ -1,10 +1,10 @@
 window.translations = {
-    "MiniOS - Fast. Simple. Reliable.": "MiniOS - Rápido. Simple. Confiable.",
+    "MiniOS - Fast. Simple. Reliable.": "MiniOS - Rápido. Sencillo. Confiable.",
     "Website": " Sitio web",
     "News": " Noticias",
     "Community": " Comunidad",
     "Documentation": " Documentación",
     "Source code": " Código fuente",
     "Thank you for choosing MiniOS.": "Gracias por elegir MiniOS.",
-    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Esperamos que disfrute de nuestro sistema. Intentamos hacer MiniOS hermoso, simple y conveniente para usted."
+    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Esperamos que disfrutes nuestro sistema. Nos esforzamos por hacer que MiniOS sea atractivo, sencillo y cómodo para ti."
 };
--- html/js/translations/ru.js
+++ html/js/translations/ru.js
@@ -1,10 +1,10 @@
 window.translations = {
-    "MiniOS - Fast. Simple. Reliable.": "MiniOS - Быстро. Просто. Надежно.",
+    "MiniOS - Fast. Simple. Reliable.": "MiniOS — быстро. Просто. Надёжно.",
     "Website": " Веб-сайт",
     "News": " Новости",
     "Community": " Сообщество",
     "Documentation": " Документация",
     "Source code": " Исходный код",
-    "Thank you for choosing MiniOS.": "Спасибо за выбор MiniOS.",
-    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Мы надеемся, что наша система вам понравится. Мы стараемся сделать MiniOS красивой, простой и удобной для вас."
+    "Thank you for choosing MiniOS.": "Спасибо, что выбрали MiniOS.",
+    "We hope you enjoy our system. We try to make MiniOS beautiful, simple and convenient for you.": "Надеемся, вам понравится наша система. Мы стараемся делать MiniOS красивой, простой и удобной для вас."
 };
